0d501937dcae48df695cc9de7c374a7ff4ac6c56
MicMetz <mime9599@colorado.edu>
2022-11-28 08:24:35 -0700

    init

--- js/headshot.js
+++ js/headshot.js
@@ -1,10 +1,12 @@
 window.onload = choosePic;
 
-var myPix = new Array("../assets/headshots/vangoghheadshot1.png", "../assets/headshots/vangoghheadshot2.png", "../assets/headshots/vangoghheadshot3.png", "../assets/headshots/vangoghheadshot4.png", "../assets/headshots/matisseheadshot.png", "../assets/headshots/watercolorheadshot.png");
+var myPix = new Array("./assets/headshots/vangoghheadshot1.png", "./assets/headshots/vangoghheadshot2.png", "./assets/headshots/vangoghheadshot3.png", "./assets/headshots/vangoghheadshot4.png", "./assets/headshots/matisseheadshot.png", "./assets/headshots/watercolorheadshot.png");
 
 function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("headshot-photo").src = myPix[randomNum];
-     console.log(myPix[randomNum]);
-     console.log("testing");
+    var randomNum = Math.floor(Math.random() * myPix.length);
+    document.getElementById("headshot-photo").src = myPix[randomNum];
+    console.log(myPix[randomNum]);
+    console.log("testing");
 }
+
+// Path: js\headshot.js
